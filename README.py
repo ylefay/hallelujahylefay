f4e10f2d7c2e64a63168f3441e2b1148e11592a2
ylf <iivodka5@gmail.com>
2023-10-24 02:07:37 +0300

    spaces

--- README.py
+++ README.py
@@ -6,9 +6,9 @@ console = Console(record=True, width=100)
 tree = Tree("[link=https://www.linkedin.com/in/yvann-le-fay/]Yvann Le Fay", guide_style="bold bright_black")
 
 self_tree = tree.add("code for me", guide_style="bright_black")
-self_tree.add("[bold link=https://github.com/hallelujahylefay/abelian_sandpile_ensae]abelian_sandpile_ensae[/] - [bright_black]dynamical system exhibiting self-organised criticality")
 self_tree.add("[bold link=https://github.com/hallelujahylefay/independent_component_analysis]independent_component_analysis[/] - [bright_black]FastICA and variational autoencoders, jax backend")
-self_tree.add("[bold link=https://github.com/hallelujahylefay/bayesianSDEsolver]bayesianSDEsolver[/] - [bright_black]efficient SDE samplers and probabilistic solvers, jax backend")
+self_tree.add("[bold link=https://github.com/hallelujahylefay/bayesianSDEsolver]bayesianSDEsolver[/] - [bright_black]efficient SDE samplers, jax backend")
+self_tree.add("[bold link=https://github.com/hallelujahylefay/abelian_sandpile_ensae]abelian_sandpile_ensae[/] - [bright_black]dynamical system exhibiting self-organised criticality")
 
 
 contrib_tree = tree.add("code I helped with", guide_style="bright_black")
@@ -20,13 +20,13 @@ future_tree.add("[bold link=https://github.com/blackjax-devs/blackjax]blackjax[/
 future_tree.add("[bold link=https://github.com/probml/dynamax]dynamax[/] - [bright_black]state space models in jax")
 
 work_tree = tree.add("work", guide_style="bright_black")
-aalto = work_tree.add("Aalto University - [bright_black]Research Assistant")
-inria = work_tree.add("INRIA - [bright_black]Research Assistant")
-lombard = work_tree.add("Lombard Odier Investment Managers - [bright_black]Quantitative Researcher")
 ens_tree = work_tree.add("École Normale Supérieure Paris-Saclay")
 ens_tree.add("M.Sc., Machine Learning (MVA)")
 ensae_tree = work_tree.add("ENSAE")
-ensae_tree.add("M.Eng, Financial Engineering and Statistics")
+ensae_tree.add("M.Eng., Financial Engineering and Statistics")
+aalto = work_tree.add("Aalto University - [bright_black]Research Assistant")
+inria = work_tree.add("INRIA - [bright_black]Research Assistant")
+lombard = work_tree.add("Lombard Odier Investment Managers - [bright_black]Quantitative Researcher")
 
 console.print(tree)
 console.print("")
